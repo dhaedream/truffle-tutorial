db2a21d944561019261e937d959fbd0cc1b644af
Dhaesha M <dhaesha1@gmail.com>
2022-03-23 10:20:34 -0400

    ready to compile smart contract

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -16,14 +16,23 @@ function adopt(uint petId) public returns (uint) {
   // we add the address that made the call (msg.sender ) to array
   adopters[petId] = msg.sender;
 
+    // reutrning pet id as confirmation 
   return petId;
 }
 
-// Retrieving the adopters to return entire array
+// array getters return only a single value from a given key. 
+// Our UI needs to update all pet adoption statuses
+// write a function to return the entire array.
+
+
 // specify the type for adopters =  address[16] memory
 // memory gives the data location for the variable
 function getAdopters() public view returns (address[16] memory) {
 
-//   Since adopters is already declared, we can simply return it  
+  //Since adopters is already declared, we can simply return it  
   return adopters;
 }
+
+// SMART CONTRACT ABOVE READY TO COMPILE 
+// In a terminal, make sure you are in the root dapp and type:
+// truffle compile
